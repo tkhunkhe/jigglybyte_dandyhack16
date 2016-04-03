bcd0b4bf3834144e87a28bf219f5b14525cb133e
tkhunkhe <tkhunkhe@gmail.com>
2016-04-03 04:11:48 -0400

    modify select date

--- Vote2Eat/app/views/pollSelectDatePage/pollSelectDatePage.js
+++ Vote2Eat/app/views/pollSelectDatePage/pollSelectDatePage.js
@@ -1,2 +1,17 @@
-var datePickerModule = require("ui/date-picker");
+ var EventViewModel = require("../../shared/view-models/event-view-model");
+var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
+var dialogsModule = require("ui/dialogs");
+var foodEvent = new EventViewModel();
 
+
+exports.loaded = function(args) {
+    var page = args.object;
+    page.bindingContext = foodEvent;
+};
+
+exports.next = function() {
+	
+	frameModule.topmost().navigate("views/pollTargetAreaPage/pollTargetAreaPage");
+
+};
