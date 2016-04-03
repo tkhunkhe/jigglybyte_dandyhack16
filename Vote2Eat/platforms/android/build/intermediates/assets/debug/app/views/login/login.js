58974d03ba56bbe3a49cde7e725dd989fdff86f0
Emma <escott19@u.rochester.edu>
2016-04-02 23:19:01 -0400

    copied grocery example into our folders

---
+++ Vote2Eat/platforms/android/build/intermediates/assets/debug/app/views/login/login.js
@@ -0,0 +1,34 @@
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var user = new UserViewModel({
+	email: "definesugar@gmail.com",
+	password: "liverspot1"
+});
+var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
+var email;
+var dialogsModule = require("ui/dialogs");
+
+exports.loaded = function(args) {
+    var page = args.object;
+    page.bindingContext = user;
+};
+
+exports.signIn = function() {
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
+};
+
+exports.register = function() {
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
+};
